271c16cf1da96dda8a3db906e98defd778d11263
daniel <4aba3691@gmail.com>
2025-03-01 20:09:41 +0100

    feat(HS-30): added reprto generator

--- routers/commands/admin_commands/admin_report_commands.py
+++ routers/commands/admin_commands/admin_report_commands.py
@@ -1,6 +1,7 @@
 from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import StateFilter
+from aiogram.types import FSInputFile
 from fsm.admin_fsm import ReportsStates, AdminStates
 from keyboards.admin_keyboards.admin_report_keyboard import *
 from keyboards.admin_keyboards.admin_keyboard import admin_keyboard
@@ -8,6 +9,7 @@ from keyboards.common_keyboard import get_confirmation_keyboard
 
 from utils.report_utils import get_monthly_report_by_month, MONTHS
 from utils.caption_utils import report_captions
+from utils.file_utils import export_monthly_report
 
 router = Router()
 
@@ -42,8 +44,9 @@ async def back_to_report_menu(callback: types.CallbackQuery, state: FSMContext):
 @router.callback_query(StateFilter(ReportsStates.monthly_report), F.data.in_(MONTHS.keys()))
 async def monthly_report_by_month(callback: types.CallbackQuery, state: FSMContext):
     month_name, month_number = MONTHS[callback.data]
+    await state.update_data(month_name=month_name, month_number=month_number)
     month_report, month_report_by_cash, month_report_by_card = await get_monthly_report_by_month(month_number)
-    await callback.message.answer(f"Отчет за {month_name}.\n{report_captions(month_report, 
+    await callback.message.answer(f"Отчет за {month_name}.\n{report_captions(month_report,
                                                                              month_report_by_cash,
                                                                              month_report_by_card)}",
                                   reply_markup=get_create_file_keyboard())
@@ -65,3 +68,12 @@ async def creation_report_file_question(callback: types.CallbackQuery, state: FS
     await state.set_state(ReportsStates.create_report_file)
 
 
+@router.callback_query(StateFilter(ReportsStates.create_report_file), F.data == "confirm")
+async def create_report_file(callback: types.CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    month_number = data.get("month_number")
+    month_name = data.get("month_name")
+    file_path = f"report_files/Отчет_{month_name}.xlsx"
+    document = FSInputFile(file_path)
+    await export_monthly_report(month_number, file_path)
+    await callback.message.answer_document(document=document, caption="Отчет за месяц")
